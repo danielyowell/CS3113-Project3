2151ba30ce97510093ad255f349ca4eab49a1981
Daniel Yowell <danielyowell@ou.edu>
2023-04-17 21:41:22 -0500

    sol6.1 has working framework

---
+++ sol6.1.c
@@ -0,0 +1,84 @@
+// @author Daniel Yowell
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+// STRUCT
+#define BUFFER_SIZE 15
+typedef struct {
+    char buffer[BUFFER_SIZE];
+    int head;
+    int tail;
+    pthread_mutex_t mutex;
+    sem_t full;
+    sem_t empty;
+} circular_buffer;
+
+// INITIALIZE CIRCULAR BUFFER
+void circular_buffer_init(circular_buffer *cb) {
+    cb->head = 0;
+    cb->tail = 0;
+    pthread_mutex_init(&cb->mutex, NULL);
+    sem_init(&cb->full, 0, BUFFER_SIZE);
+    sem_init(&cb->empty, 0, 0);
+}
+
+//? HELPER METHODS
+
+// write to buffer (do NOT print)
+int circular_buffer_write(circular_buffer *cb, char c) {
+
+}
+
+// extract character from buffer, move up head of buffer
+char circular_buffer_read(circular_buffer *cb) {
+
+}
+
+//! BIG BOYS
+
+void* readfile_writebuffer(void* arg) {
+
+}
+
+// CONSUMER
+void* readbuffer_writeoutput(void* arg) {
+
+}
+
+// MAIN FUNCTION
+int main() {
+    // Create pthread objects
+    pthread_t write_thread, read_thread;
+    
+    // Initialize circular buffer
+    circular_buffer cb;
+    circular_buffer_init(&cb);
+
+    // CREATE PRODUCER: reads from file, writes to buffer
+    pthread_create(&write_thread, NULL, readfile_writebuffer, &cb);
+
+    // Sleep for 0.5 seconds
+    usleep(100000);
+    
+    // CREATE CONSUMER: reads from buffer, writes to output
+    pthread_create(&read_thread, NULL, readbuffer_writeoutput, &cb);
+
+    pthread_join(write_thread, NULL);
+    pthread_join(read_thread, NULL);
+    printf("\n");
+    return 0;
+}
